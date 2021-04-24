d059b5747709f0b9f843d470f73d4422a97072c5
aliyussef <a.yussef@outlook.com>
2021-04-24 18:07:28 +0200

    Added BootStrap class for storing and loading data from h2 database.

---
+++ src/main/java/com/goldenshoe/onlinestore/bootstrap/BootStrap.java
@@ -0,0 +1,16 @@
+package com.goldenshoe.onlinestore.bootstrap;
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.stereotype.Component;
+
+/**
+ * Created by aliyussef on 24/04/2021
+ */
+@Component
+public class BootStrap implements CommandLineRunner {
+
+    @Override
+    public void run(String... args) throws Exception {
+
+    }
+}
