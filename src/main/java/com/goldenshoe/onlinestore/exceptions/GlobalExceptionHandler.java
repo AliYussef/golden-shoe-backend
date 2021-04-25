8ae026f17b3ca78237b24106a091bfd165cef7cf
aliyussef <a.yussef@outlook.com>
2021-04-25 17:25:35 +0200

    Added ResourceNotFoundException and handling in GlobalExceptionHandler.

--- src/main/java/com/goldenshoe/onlinestore/exceptions/GlobalExceptionHandler.java
+++ src/main/java/com/goldenshoe/onlinestore/exceptions/GlobalExceptionHandler.java
@@ -1,7 +1,13 @@
 package com.goldenshoe.onlinestore.exceptions;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+import org.springframework.web.context.request.WebRequest;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
+
+import java.util.Date;
 
 /**
  * Created by aliyussef on 24/04/2021
@@ -9,4 +15,27 @@ import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExcep
 @RestControllerAdvice
 public class GlobalExceptionHandler extends ResponseEntityExceptionHandler {
 
+    @ExceptionHandler(Exception.class)
+    public final ResponseEntity<Object> handleAllExceptions(Exception exception, WebRequest webRequest) {
+        ErrorDetails errorDetails = ErrorDetails.builder()
+                .timestamp(new Date())
+                .message(exception.getMessage())
+                .details(webRequest.getDescription(false))
+                .statusCode(HttpStatus.INTERNAL_SERVER_ERROR.value())
+                .build();
+
+        return new ResponseEntity<>(errorDetails, HttpStatus.INTERNAL_SERVER_ERROR);
+    }
+
+    @ExceptionHandler(ResourceNotFoundException.class)
+    public final ResponseEntity<Object> handleNotFoundException(Exception exception, WebRequest webRequest) {
+        ErrorDetails errorDetails = ErrorDetails.builder()
+                .timestamp(new Date())
+                .message(exception.getMessage())
+                .details(webRequest.getDescription(false))
+                .statusCode(HttpStatus.NOT_FOUND.value())
+                .build();
+
+        return new ResponseEntity<>(errorDetails, HttpStatus.NOT_FOUND);
+    }
 }
