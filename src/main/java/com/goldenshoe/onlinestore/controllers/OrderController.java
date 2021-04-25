4e881eb9c351144fda5a846a92d57ee9c52b4ac7
aliyussef <a.yussef@outlook.com>
2021-04-25 17:27:56 +0200

    Added controller classes.

---
+++ src/main/java/com/goldenshoe/onlinestore/controllers/OrderController.java
@@ -0,0 +1,12 @@
+package com.goldenshoe.onlinestore.controllers;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * Created by aliyussef on 25/04/2021
+ */
+@RestController
+@RequestMapping("api/v1/orders")
+public class OrderController {
+}
