82bf1f989e04f76e9896231902043c7b1c54f1a8
aliyussef <a.yussef@outlook.com>
2021-05-03 20:28:18 +0200

    Added date extension.

---
+++ src/main/java/com/goldenshoe/onlinestore/utils/DateExtension.java
@@ -0,0 +1,17 @@
+package com.goldenshoe.onlinestore.utils;
+
+import java.time.LocalDate;
+import java.time.ZoneId;
+import java.util.Date;
+
+/**
+ * Created by aliyussef on 02/05/2021
+ */
+public final class DateExtension {
+
+    public static LocalDate convertToLocalDateViaInstant(Date dateToConvert) {
+        return dateToConvert.toInstant()
+                .atZone(ZoneId.systemDefault())
+                .toLocalDate();
+    }
+}
